2a3514479ea6de01ce2aaf49bb95e0b65bdeac45
Himanshu <himanshu.sharma11199@gmail.com>
2021-02-21 13:17:51 +0530

    latest update

--- app.js
+++ app.js
@@ -126,10 +126,9 @@ app.get("/work", (req, res)=> {
 });
 
 let port = process.env.PORT;
-if(port == null || port == ""){
+if(port == null || port == ""{
   port = 3000;
-}
-
+})
 app.listen(port, ()=> {
   console.log("server is ready");
 });
